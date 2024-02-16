948d776f088fe503f039ca1a8aa79d7bff2c20f8
980pro <dnjswo8820@gmail.com>
2024-02-16 11:41:36 +0900

    hello world 출력

--- src/main/java/com/organizations/practice3/Practice3Application.java
+++ src/main/java/com/organizations/practice3/Practice3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Practice3Application {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(Practice3Application.class, args);
     }
 
